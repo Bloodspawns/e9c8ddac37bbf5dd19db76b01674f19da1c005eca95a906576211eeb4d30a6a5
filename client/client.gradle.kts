e5f621456559e72df5f69ff50154d557fc2893b5
unknown <you@example.com>
2020-09-23 22:42:03 +0200

    fix classpath order with dependencies

--- client/client.gradle.kts
+++ client/client.gradle.kts
@@ -11,41 +11,84 @@ repositories {
 
 val client = configurations.create("client")
 val plugins = configurations.create("plugins")
-val patches = configurations.create("patches")
-val runclient = configurations.create("runclient")
-
+val externals = configurations.create("externals")
+val baseRuneLite = configurations.create("baseRuneLite")
+val baseBlueLite = configurations.create("baseBlueLite")
+val repackedClient = configurations.create("repackedClient")
 dependencies {
     client(Libraries.runelite_client)
-    runclient(Libraries.jcodec_javase)
-    runclient(Libraries.jcodec)
-    runclient(files(client.files.filter { !it.name.startsWith("client") }))
-    runclient(files("build/libs/bclient-1.0.jar"))
+    baseBlueLite(fileTree(project.gradle.gradleUserHomeDir.parent + "/.runelite/bluerepo/"){
+        include("*.jar") }.filter{ !isRLClient(it.name) && !isRLApi(it.name) && !isRLHttpApi(it.name)})
+    baseRuneLite(files(client.files.filter { !isRLClient(it.name) }))
+    repackedClient(files("build/libs/bclient-1.0.jar"))
     plugins(project(":plugins")) { isTransitive = false }
-    plugins(fileTree(project.gradle.gradleUserHomeDir.parent + "/.runelite/bluerepo/"){ include("*.jar") })
-    runclient(fileTree(project.gradle.gradleUserHomeDir.parent + "/.runelite/bexternalplugins/"){ include("*.jar") })
-    patches(files(project.gradle.gradleUserHomeDir.parent + "/.runelite/bluerepo/blue-patch.jar"))
+    externals(fileTree(project.gradle.gradleUserHomeDir.parent + "/.runelite/bexternalplugins/"){
+        include("*.jar") })
 }
 
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_8
 }
 
-//runclient.files.filter { it.name.startsWith("client") }.forEach{println(it.name)}
+fun isRLClient(name : String): Boolean {
+    return Regex("client-([0-9]*\\.)*[0-9]*\\.jar").matches(name)
+}
+
+fun isRLApi(name : String): Boolean {
+    return Regex("http-api-([0-9]*\\.)*[0-9]*\\.jar").matches(name)
+}
+
+fun isRLHttpApi(name : String): Boolean {
+    return Regex("runelite-api-([0-9]*\\.)*[0-9]*\\.jar").matches(name)
+}
+
+fun printClasspath(rl : Boolean) {
+    fun names(set : Set<File>) : String {
+        return set.joinToString(", ") { it.name }
+    }
+    println("--------------------------------------------------------")
+    println("JAR files on the classpath in order of precedence are:")
+    println("Plugins: " + names(plugins.files))
+    println("Externals: " + names(externals.files))
+    println("Repacked client: " + names(repackedClient.files))
+    if (!rl) {
+        println("BlueLite: " + names(baseBlueLite.files))
+    }
+    println("RuneLite: " + names(baseRuneLite.files))
+    println("--------------------------------------------------------")
+}
 
 tasks {
 
     register<Jar>("repack") {
+        doFirst {
+            println("Repacking " + client.first().name + " into bclient-1.0.jar")
+        }
         from(zipTree(client.first())) {
             exclude("META-INF/")
         }
         baseName = "bclient"
     }
 
-    register<JavaExec>("Client.main()") {
-//        dependsOn(":client:repack")
+    register<JavaExec>("BlueLite.main()") {
+        doFirst {
+            printClasspath(false)
+        }
         group = "run"
 
-        classpath = plugins.plus(patches).plus(runclient)
+        classpath = plugins.plus(externals).plus(repackedClient).plus(baseBlueLite).plus(baseRuneLite)
+        main = "net.runelite.client.RuneLite"
+        jvmArgs = listOf("-ea")
+        args = listOf("--developer-mode", "--debug")
+    }
+
+    register<JavaExec>("Runelite.main()") {
+        doFirst {
+            printClasspath(true)
+        }
+        group = "run"
+
+        classpath = plugins.plus(externals).plus(repackedClient).plus(baseRuneLite)
         main = "net.runelite.client.RuneLite"
         jvmArgs = listOf("-ea")
         args = listOf("--developer-mode", "--debug")
